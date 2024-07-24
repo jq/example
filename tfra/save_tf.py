8743cee56ed632c03d2a93b20d3f737ff217b156
Julian Qian <jqian2@snapchat.com>
2024-07-23 17:59:54 -0700

    save with tfra

--- tfra/save_tf.py
+++ tfra/save_tf.py
@@ -1,5 +1,6 @@
 import tensorflow as tf
 from tensorflow import keras
+from tensorflow_recommenders_addons import dynamic_embedding as de
 
 class MyModel(keras.Model):
     def __init__(self):
@@ -21,6 +22,10 @@ class MyModel(keras.Model):
         x = self.postprocess(x)
         return self.dense2(x)
 
+@tf.function
+def model_function(inputs):
+    x = inputs * 10
+    return model(x)
 
 model = MyModel()
 #model.build((None, 2)) this is similar to input_shape=(2,)
@@ -28,13 +33,20 @@ test_input = tf.random.normal([1, 2])
 model(test_input)
 
 path_to_save = '/tmp/my_model'
-tf.keras.models.save_model(model, path_to_save)
+input_spec = tf.TensorSpec([None, 2], dtype=tf.float32)
+
+de.keras.models.save_model(model, path_to_save,
+                           signatures={'serving_default': model_function.get_concrete_function(input_spec)})
+#tf.keras.models.save_model(model, path_to_save)
 
 
 loaded_model = tf.keras.models.load_model(path_to_save, custom_objects={'MyModel': MyModel})
-
 test_input = tf.constant([[1.0, 2.0]])
-
 output = loaded_model(test_input)
 
 print("Output of the loaded model:", output.numpy())
+
+loaded_function = loaded_model.signatures['serving_default']
+
+print("Output from loaded function:", loaded_function(test_input))
+
